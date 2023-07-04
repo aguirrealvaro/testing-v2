753eb7aa075c51a473dfb6ba39ea03195cba12d5
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-04 16:21:33 -0300

    Add more test to fetch.tsx

--- src/components/Fetch/Fetch.test.tsx
+++ src/components/Fetch/Fetch.test.tsx
@@ -1,5 +1,4 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Fetch } from "./Fetch";
 
 describe("Fetch tests", () => {
@@ -17,6 +16,32 @@ describe("Fetch tests", () => {
   it("when button is clicked, loading status is displayed", async () => {
     render(<Fetch />);
     const button = screen.getByRole("button", { name: /load users/i });
-    await userEvent.click(button);
+    // idk it is now working
+    //await userEvent.click(button);
+    fireEvent.click(button);
+    const loading = screen.getByTestId("loading-status");
+    expect(loading).toBeInTheDocument();
+  });
+  it("when button is clicked, we wait until heading displays", async () => {
+    render(<Fetch />);
+    const button = screen.getByRole("button", { name: /load users/i });
+    fireEvent.click(button);
+    const heading = await screen.findByRole("heading", { level: 2 });
+    expect(heading).toBeInTheDocument();
+  });
+  it("when button is clicked, we wait until list displays", async () => {
+    render(<Fetch />);
+    const button = screen.getByRole("button", { name: /load users/i });
+    fireEvent.click(button);
+    const list = await screen.findByRole("list");
+    expect(list).toBeInTheDocument();
+  });
+  it("when button is clicked, we wait until list displays, and loading disappear", async () => {
+    render(<Fetch />);
+    const button = screen.getByRole("button", { name: /load users/i });
+    fireEvent.click(button);
+    await screen.findByRole("list");
+    const loading = screen.queryByTestId("loading-status");
+    expect(loading).not.toBeInTheDocument();
   });
 });

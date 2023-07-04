ea19dd1a79f0ad12d70de6d12f3ba4146f336611
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-04 14:21:59 -0300

    Test list is not initial renderer

--- src/components/Fetch/Fetch.test.tsx
+++ src/components/Fetch/Fetch.test.tsx
@@ -5,6 +5,12 @@ describe("Fetch tests", () => {
   it("Button to load users is rendered", () => {
     render(<Fetch />);
     const button = screen.getByRole("button", { name: /load users/i });
+    expect(button).toHaveTextContent("Load users");
     expect(button).toBeInTheDocument();
+  });
+  it("List is not initially renderer", () => {
+    render(<Fetch />);
+    const list = screen.queryByRole("list");
+    expect(list).not.toBeInTheDocument();
   });
 });

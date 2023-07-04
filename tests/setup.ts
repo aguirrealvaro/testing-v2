28d4f72154cd28b1c878a4dd7039063956eb9bbe
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2023-07-04 17:09:07 -0300

    Remove cleanup as it is not needed anymore

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,16 +1,9 @@
 import matchers from "@testing-library/jest-dom/matchers";
-//import { cleanup } from "@testing-library/react";
 import { expect, afterEach } from "vitest";
 import { server } from "../src/mocks/server";
 
 // extends Vitest's expect method with methods from react-testing-library
 expect.extend(matchers);
-
-// without msw
-// runs a cleanup after each test case (e.g. clearing jsdom)
-/* afterEach(() => {
-  cleanup();
-}); */
 
 beforeAll(() => server.listen());
 afterAll(() => server.close());
